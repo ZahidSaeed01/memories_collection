f9f14dce49c55082ab4f8c84dd9b7ecdc053329a
Zahid Saeed <muhammadzahid1099@gmail.com>
2021-05-16 14:04:29 +0500

    add auth

--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ import mongoose from "mongoose";
 import cors from "cors";
 
 import postRoutes from "./routes/posts.js";
+import userRouter from "./routes/user.js";
 
 const app = express();
 
@@ -12,6 +13,7 @@ app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
 app.use("/posts", postRoutes);
+app.use("/user", userRouter);
 
 //const CONNECTION_URL = 'mongodb+srv://js_mastery:123123123@practice.jto9p.mongodb.net/test';
 const CONNECTION_URL = "mongodb://localhost/memories";
